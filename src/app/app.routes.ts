a6a8a87c715039e63c4ae071d060ff8bfa9495da
Martin Großmann <martin.grossmann@telekom.de>
2023-12-08 16:34:21 +0100

    Created app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import {DownloadMilestoneComponent} from "./download-milestone/download-milestone.component";
+import {AboutComponent} from "./about/about.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: '/milestone'
+  },
+  {
+    path: 'milestone',
+    component: DownloadMilestoneComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  }
+];
---
+++ src/app/download-milestone/download-milestone.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-download-milestone',
+  standalone: true,
+  imports: [],
+  templateUrl: './download-milestone.component.html',
+  styleUrl: './download-milestone.component.scss'
+})
+export class DownloadMilestoneComponent {
+
+}
