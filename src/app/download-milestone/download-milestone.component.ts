2ef43771c6ff4109e4bae830c9190d12704eeac8
Martin Großmann <martin.grossmann@telekom.de>
2023-12-11 11:32:11 +0100

    Added service

--- src/app/download-milestone/download-milestone.component.ts
+++ src/app/download-milestone/download-milestone.component.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {MatTableModule} from "@angular/material/table";
 import {MatCardModule} from "@angular/material/card";
+import {PeriodicElement} from "../models/models";
+import {DataService} from "../services/data.service";
+import {Observable, subscribeOn, Subscription} from "rxjs";
 
 @Component({
   selector: 'app-download-milestone',
@@ -10,31 +13,24 @@ import {MatCardModule} from "@angular/material/card";
     MatCardModule
   ],
   templateUrl: './download-milestone.component.html',
-  styleUrl: './download-milestone.component.scss'
+  styleUrl: './download-milestone.component.scss',
+  providers: [DataService]
 })
-export class DownloadMilestoneComponent {
+
+export class DownloadMilestoneComponent implements OnInit{
   displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = ELEMENT_DATA;
+  dataSource!: PeriodicElement[]
+  subscription!: Subscription;
+
+
+  constructor(private dataService: DataService) {
+  }
+
+  ngOnInit() {
+    this.dataService.getTableData().subscribe(elements => this.dataSource = elements);
+  }
 }
 
-// Just for demo
 
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-];
+
